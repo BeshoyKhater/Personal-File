a8b14075bd86a4991c726ff89a4c71c86035fdfe
BeshoyKhater <BeshoyKhater@gmail.com>
2023-05-22 15:25:10 +0300

    edit website

--- src/data/data.js
+++ src/data/data.js
@@ -169,24 +169,24 @@ export const skills = [
   {
     id: 1,
     num: 90,
-    text: "HTML",
+    text: "HTML5",
     class: "skill1",
   },
   {
     id: 2,
     num: 85,
-    text: "CSS",
+    text: "CSS3",
     class: "skill2",
   },
   {
     id: 3,
-    num: 70,
+    num: 75,
     text: "JavaScript",
     class: "skill3",
   },
   {
     id: 4,
-    num: 80,
+    num: 90,
     text: "SCSS",
     class: "skill4",
   },
@@ -198,7 +198,7 @@ export const skills = [
   },
   {
     id: 6,
-    num: 75,
+    num: 85,
     text: "React",
     class: "skill6",
   },
@@ -207,6 +207,18 @@ export const skills = [
     num: 70,
     text: "GitHub",
     class: "skill7",
+  },
+  {
+    id: 8,
+    num: 70,
+    text: "Typescript",
+    class: "skill8",
+  },
+  {
+    id: 9,
+    num: 80,
+    text: "Next",
+    class: "skill9",
   },
 ]
 const Avada=require("../assets/images/Avada.jpg");
@@ -219,18 +231,20 @@ const cart=require("../assets/images/cart.jpg");
 const sportSite=require("../assets/images/sport-site.jpg");
 const portfolio=require("../assets/images/portfolio.jpg");
 const PersonalFile=require("../assets/images/Personal-File.jpg");
+const dashboard=require("../assets/images/dashboard.jpg");
 
 export const projects =[
-    {id:1 , image:Avada , date:"20/6/2022" , github:"https://github.com/BeshoyKhater/Avada" , demo:"https://avada2022.netlify.app/" ,  type:"UI", title:"Avada (UI)" , desc:"Html-Css-Bootstrap "},
-    {id:2 , image:iphone , date:"8/7/2022" , github:"https://github.com/BeshoyKhater/IPhone" , demo:"https://iphone2022.netlify.app" ,  type:"UI", title:"IPhone (UI)" , desc:"Html-Css-Javascript"},
-    {id:3 , image:developYourSkills , date:"12/8/2022" , github:"https://github.com/BeshoyKhater/Develop-your-skills" , demo:"https://develop-your-skills.netlify.app" ,  type:"UI", title:"Develop Your Skills (UI)" , desc:'Html-Css-Javascript "Responsive"'},
-    {id:4 , image:education , date:"1/9/2022" , github:"https://github.com/BeshoyKhater/Education-Website" , demo:"https://education-website2022.netlify.app" ,  type:"UI", title:"Education Website (UI)" , desc:'Html-Sass-Javascript "Responsive"'},
-    {id:5 , image:healthCare , date:"30/9/2022" , github:"https://github.com/BeshoyKhater/Health-Care" , demo:"https://health-care2023.netlify.app" ,  type:"UI", title:"Health Care (UI)" , desc:'Html-Sass-Javascript "Responsive"'},
-    {id:6 , image:Avada2 , date:"15/10/2022" , github:"https://github.com/BeshoyKhater/Avada2" , demo:"https://avada2.netlify.app" ,  type:"react", title:"Avada2 (React)" , desc:"React-Css-Javascript"},
-    {id:7 , image:cart , date:"1/11/2022" , github:"https://github.com/BeshoyKhater/Cart" , demo:"https://cart2023.netlify.app" ,  type:"react", title:"Cart (React)" , desc:"React-Css-Redux Toolkit"},
-    {id:8 , image:sportSite , date:"1/12/2022" , github:"https://github.com/BeshoyKhater/Sport-Site" , demo:"https://sport-site2023.netlify.app" ,  type:"react", title:"Sport Site (React)" , desc:'React-Sass-Bootstrap "Responsive"'},
-    {id:9 , image:portfolio , date:"1/1/2023" , github:"https://github.com/BeshoyKhater/Portfolio" , demo:"https://portfolio-react2023.netlify.app" ,  type:"react", title:"Portfolio (React)" , desc:'React-Sass-Bootstrap "Responsive"'},
+    {id:1 , image:Avada , date:"20/6/2022" , github:"https://github.com/BeshoyKhater/Avada" , demo:"https://beshoykhater.github.io/Avada/" ,  type:"UI", title:"Avada (UI)" , desc:"Html-Css-Bootstrap "},
+    {id:2 , image:iphone , date:"8/7/2022" , github:"https://github.com/BeshoyKhater/IPhone" , demo:"https://beshoykhater.github.io/IPhone/" ,  type:"UI", title:"IPhone (UI)" , desc:"Html-Css-Javascript"},
+    {id:3 , image:developYourSkills , date:"12/8/2022" , github:"https://github.com/BeshoyKhater/Develop-your-skills" , demo:"https://beshoykhater.github.io/Develop-your-skills/" ,  type:"UI", title:"Develop Your Skills (UI)" , desc:'Html-Css-Javascript "Responsive"'},
+    {id:4 , image:education , date:"1/9/2022" , github:"https://github.com/BeshoyKhater/Education-Website" , demo:"https://beshoykhater.github.io/Education-Website/" ,  type:"UI", title:"Education Website (UI)" , desc:'Html-Sass-Javascript "Responsive"'},
+    {id:5 , image:healthCare , date:"30/9/2022" , github:"https://github.com/BeshoyKhater/Health-Care" , demo:"https://beshoykhater.github.io/Health-Care/" ,  type:"UI", title:"Health Care (UI)" , desc:'Html-Sass-Javascript "Responsive"'},
+    {id:6 , image:Avada2 , date:"15/10/2022" , github:"https://github.com/BeshoyKhater/Avada2" , demo:"https://beshoykhater.github.io/Avada2/" ,  type:"react", title:"Avada2 (React)" , desc:"React-Css-Javascript"},
+    {id:7 , image:cart , date:"1/11/2022" , github:"https://github.com/BeshoyKhater/Cart" , demo:"https://beshoykhater.github.io/Cart/" ,  type:"react", title:"Cart (React)" , desc:"React-Css-Redux Toolkit"},
+    {id:8 , image:sportSite , date:"1/12/2022" , github:"https://github.com/BeshoyKhater/Sport-Site" , demo:"https://beshoykhater.github.io/Sport-Site/" ,  type:"react", title:"Sport Site (React)" , desc:'React-Sass-Bootstrap "Responsive"'},
+    {id:9 , image:portfolio , date:"1/1/2023" , github:"https://github.com/BeshoyKhater/Portfolio" , demo:"https://beshoykhater.github.io/Portfolio/" ,  type:"react", title:"Portfolio (React)" , desc:'React-Sass-Bootstrap "Responsive"'},
     {id:10 , image:PersonalFile , date:"5/2/2023" , github:"https://github.com/BeshoyKhater/Personal-File" , demo:"https://personal-file2023.netlify.app" ,  type:"react", title:"Personal File (React)" , desc:'React-Sass-Bootstrap "Responsive"'},
+    {id:11 , image:dashboard , date:"5/22/2023" , github:"https://github.com/BeshoyKhater/DASHBOARD" , demo:"https://beshoykhater.github.io/DASHBOARD/" ,  type:"react", title:"Dashboard (React)" , desc:'React-MUI-FullCalendar-Nivo "Responsive"'},
 ]
 
 export const buttons = [
